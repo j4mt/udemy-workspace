8ef810aba6bf3e12e9eb26ebf413f07c11fec303
j4mt <john.orourke.1985@gmail.com>
2020-05-23 09:55:01 +0100

    Finishing assignement 1 corrected

--- my-first-app/src/app/success-alert/success-alert.component.ts
+++ my-first-app/src/app/success-alert/success-alert.component.ts
@@ -2,8 +2,18 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-success-alert',
-  templateUrl: './success-alert.component.html',
-  styleUrls: ['./success-alert.component.css']
+  template: `
+  <p>This is a Success, you are in Successful!</p>
+  `,
+    styles: [
+    `
+      p {
+      padding: 20px;
+      background-color: palegreen;
+      border: 1px solid green;
+      }
+    `
+    ]
 })
 export class SuccessAlertComponent implements OnInit {
 
--- my-first-app/src/app/warning-alert/warningalert.component.ts
+++ my-first-app/src/app/warning-alert/warningalert.component.ts
@@ -1,9 +1,19 @@
 import { Component } from '@angular/core';
 
 @Component({
-selector: 'app-warning-alert',
-templateUrl: './warningalert.component.html',
-styleUrls: ['./warningalert.component.css']
+  selector: 'app-warning-alert',
+  template: `
+      <p>This is a warning, you are in Danger</p>
+    `,
+    styles: [
+      `
+      p {
+        padding: 20px;
+        background-color: mistyrose;
+        border: 1px solid red;
+      }
+    `
+    ]
 })
 export class WarningAlertComponent{
 }
