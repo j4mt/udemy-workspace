d3ebb78099c7a4d26300ff41e743b02e076112b9
j4mt <john.orourke.1985@gmail.com>
2020-05-27 15:19:20 +0100

    Updating with bi direactional data binding

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -2,14 +2,32 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: '[app-servers]',
-  template: `<app-server></app-server>`,
+//  template: `<app-server></app-server>`,
+templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
+
 })
 export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created!';
+  serverName = 'Test Server';
 
-  constructor() { }
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
 
   ngOnInit(): void {
+  }
+
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was created! : ' + this.serverName;
+  }
+
+  onUpdateServerName(event: any) {
+      console.log(event);
+      this.serverName = (<HTMLInputElement>event.target).value;
   }
 
 }
